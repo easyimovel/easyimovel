eb7ca201a1668af16accfc757547808b842b1438
Murilo <muhbianco@gmail.com>
2025-02-06 15:24:37 +0000

    new structure

---
+++ app/main.py
@@ -0,0 +1,80 @@
+import logging
+
+from dotenv import load_dotenv
+load_dotenv()
+
+from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi_versioning import VersionedFastAPI
+from fastapi.staticfiles import StaticFiles
+
+from app.routers import web_scrappy
+from app.routers import auth
+
+from app.utils.auth import get_current_user
+from app.utils.db import DB
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler("app.log"),
+        logging.StreamHandler()
+    ]
+)
+logger = logging.getLogger("EasyImovelAPI")
+
+description = """EasyImovelAPI"""
+summary = "API EasyImovel"
+
+tags_metadata = [
+    {"name": "EasyImovelScrapper", "description": "Web scrappy"},
+]
+
+app_base = FastAPI(
+    title=description,
+    debug=True,
+)
+
+app_base.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app_base.include_router(
+    auth.router,
+    tags=["EasyAuth"],
+    dependencies=[Depends(DB)]
+)
+
+# WebScrapp
+app_base.include_router(
+    web_scrappy.router,
+    tags=["EasyImovelScrapper"],
+    prefix="/web_scrappy",
+    dependencies=[Depends(get_current_user), Depends(DB)]
+)
+
+app = VersionedFastAPI(
+    app_base,
+    enable_latest=True,
+    summary=summary,
+    description=description,
+    openapi_tags=tags_metadata,
+    debug=True,
+)
+
+# @app.on_event("startup")
+# async def startup_event():
+#     await QuestionNPC.init_driver()
+
+# @app.on_event("shutdown")
+# def shutdown_event():
+#     QuestionNPC.close()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+logger.info("Static files mounted at /static")
---
+++ app/routers/web_scrappy.py
@@ -0,0 +1,53 @@
+import uuid
+import scrapy
+import multiprocessing
+import os
+import re
+import requests
+import logging
+import smtplib
+import ssl
+
+from email.message import EmailMessage
+from datetime import datetime, date
+from typing_extensions import TypedDict
+from typing import List, Annotated, Any, Union, Tuple
+from pprint import pprint, pformat
+from urllib.parse import urlencode, urljoin, urlparse, parse_qs, urlunparse, unquote
+from fnmatch import fnmatch
+
+from fastapi import APIRouter, Request, status, Body, HTTPException, Query
+from fastapi_versioning import version
+from fastapi.responses import FileResponse, JSONResponse
+
+from scrapy.crawler import CrawlerProcess
+from scrapy.linkextractors import LinkExtractor
+from scrapy.settings import Settings
+from scrapy_splash import SplashRequest
+
+from twisted.internet import asyncioreactor
+
+from app.utils.db import get_session
+from app.util.auth import get_users
+
+
+logger = logging.getLogger("EasyImovelAPI")
+router = APIRouter()
+
+prefix_dir = "/mnt/scrappers"
+
+
+
+
+@router.post(
+    "/get_editals",
+    status_code=status.HTTP_200_OK,
+    response_model=None,
+)
+@version(1, 0)
+def get_editals(
+    request: Request,
+    db = Depends(get_session),
+) -> Union[List[FileResponse], HTTPException, JSONResponse]:
+    print(get_users(db))
+    return JSONResponse(status_code=201, content={"detail": "Process successfully started."})
